62c58d77ae3b33c4d4c42b90de17eb78afdaf2ba
Duroxxigar <young9003@gmail.com>
2019-09-22 17:46:33 -0400

    Completed skeleton of list

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var button = document.getElementById('add-btn');
-button.addEventListener('click', addItem);
+var addBtn = document.getElementById('add-btn');
+var taskList = document.getElementById('task-list');
+var taskInput = document.getElementById('task-to-add');
+addBtn.addEventListener('click', addItem);
 
 function addItem() {
-    alert("hello!");
+    var li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskInput.value));
+    taskList.appendChild(li)
+    taskInput.value = '';
 }
