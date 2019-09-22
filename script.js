787c2460317f22bb08041535d363f3209cdc77bd
Duroxxigar <young9003@gmail.com>
2019-09-22 18:59:07 -0400

    Added ability to press enter to add to the list. Also can remove items by clicking on the item.

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ var addBtn = document.getElementById('add-btn');
 var taskList = document.getElementById('task-list');
 var taskInput = document.getElementById('task-to-add');
 addBtn.addEventListener('click', addItem);
+taskInput.addEventListener('keyup', enterPressed);
 
 function addItem() {
-    var li = document.createElement('li')
+    var li = document.createElement('li');
     li.appendChild(document.createTextNode(taskInput.value));
-    taskList.appendChild(li)
+    li.style.cursor = 'pointer';
+    taskList.appendChild(li);
     taskInput.value = '';
+    li.addEventListener('click', function() {
+        this.parentNode.removeChild(this);
+    });
+}
+
+function enterPressed(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        addBtn.click();
+    }
 }
